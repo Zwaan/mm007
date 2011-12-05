86eec0dd9ead7fc311908940cc1cf12c8b557d5e
165_goethals <thijs.goethals@hotmail.com>
2011-12-05 10:58:30 +0100

    rmse function completed. Now takes input as inline and poly.

--- rmse.m
+++ rmse.m
@@ -1,7 +1,10 @@
-function [ output_args ] = rmse( input_args )
-%RMSE Summary of this function goes here
-%   Detailed explanation goes here
-
-
+function rmse=rmse(F,X,Y)
+% F: function inline or poly
+% X: matrix with x values
+% Y: matrix with y values
+% bv: F=inline('x^2 + 3*x + 5');
+% RMSE(F,[1 2 3],[1 2 5])
+if strcmp(class(F),'double'); F=poly2sym(F);end
+F=vectorize(F);
+rmse=sqrt(mean((F(X)-Y).^2));
 end
-
