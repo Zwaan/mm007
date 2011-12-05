2f71fabbeddaf91b496d77c22e871f634a6e1b48
165_goethals <thijs.goethals@hotmail.com>
2011-12-06 00:57:28 +0100

    Addapted to make it work with systems of equations. Comments will be added later

--- eulerdiff.m
+++ eulerdiff.m
@@ -1,8 +1,10 @@
 function [x,y] = eulerdiff(f,x0,y0,h,n)
 x = (x0 + h : h: x0 + n*h);
-y(1) = y0 + h*feval(f,x0,y0);
+yt=num2cell(y0);
+y(1,:) = y0 + h*feval(f,x0,yt{:})';
 for i = 2 : n
-   y(i) = y(i-1) + h*feval(f,x(i-1),y(i-1));
+  yt=num2cell(y(i-1,:));
+  y(i,:) = y(i-1,:) + h*feval(f,x(i-1),yt{:})';
 end
 x = [x0 x]; 
-y = [y0 y];
+y = [y0' y'];
