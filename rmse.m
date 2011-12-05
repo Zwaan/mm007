9a679fecbb72d882af2edf7bdfe014710f7ad326
165_goethals <thijs.goethals@hotmail.com>
2011-12-05 11:13:23 +0100

    Comments added

--- rmse.m
+++ rmse.m
@@ -4,7 +4,7 @@ function rmse=rmse(F,X,Y)
 % Y: matrix with y values
 % bv: F=inline('x^2 + 3*x + 5');
 % RMSE(F,[1 2 3],[1 2 5])
-if strcmp(class(F),'double'); F=poly2sym(F);end
-F=vectorize(F);
-rmse=sqrt(mean((F(X)-Y).^2));
+if strcmp(class(F),'double'); F=poly2sym(F);end%checks if poly and converts to symbolic
+F=vectorize(F);%vectorizes F for dealing with matrices.
+rmse=sqrt(mean((F(X)-Y).^2));%Application of formule.
 end
