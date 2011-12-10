cb30d083cf757d0ac3bc5fa81edf84587d1777e0
165_goethals <thijs.goethals@hotmail.com>
2011-12-10 16:39:56 +0100

    added trapezium

--- trapezium.m
+++ trapezium.m
@@ -1,5 +1,9 @@
 function I = trapezium(f,a,b,n)
-h=(b-a)/n;
-x=a:h:b;
+%f: inline function
+%a: start value
+%b: end value
+%n: number of intervals
+x=a:(b-a)/n:b;
+f=vectorize(f);% vectorizes f for working with arrays in feval.
 y=feval(f,x);
 I=h/2*(y(1)+2*sum(y(2:n))+y(n+1));
