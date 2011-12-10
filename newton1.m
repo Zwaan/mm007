7f00371147124afda92a2dd7988f7d1a74129155
165_goethals <thijs.goethals@hotmail.com>
2011-12-10 16:41:32 +0100

    added comments to newton1, newtons + minor improvements

--- newton1.m
+++ newton1.m
@@ -1,16 +1,20 @@
-function x = newton1(f,df,x0,delta,maxit)
+function x = newton1(f,df,x0,tol,maxit)
 % f 		: filename of the function
 % df		: filename of the derivative
 % x0		: starting value
-% delta 	: value for convergence test
+% tol       : value for convergence test
 % maxit	    : maximum number of iterations
 % RETURN    : x = vector [x0 x1 x2... xn]
+
+%example:
+%y=inline('x^3+3*x-4','x');
+%dy=inline('3*x^2+3','x'); 
+%N=newton1(y,dy,3,10^-20,55);
 x = x0;
 for n = 1 : maxit
     x(n+1) = x(n) - feval(f,x(n))/(feval(df,x(n)));
-    if abs(feval(f,x(n+1))) < delta
+    if abs(feval(f,x(n+1))) < tol % y=f(x(n+1)) moet nul worden, dus kleiner worden dan tolerantie
         disp('Convergence test : OK');
         return; % stops the function and returns x
     end
 end
-disp('Convergence test : NOT OK');
--- newtons.m
+++ newtons.m
@@ -1,16 +1,27 @@
-function  p = newtons(f,df,p0,delta,maxit)
-% f		    : filename of the system
-% df		: filename of the Jacobian
-% p0		: starting point : column vector [x0, y0, z0,...]'
-% delta 	: value for convergence test
-% maxit 	: maximum number of iterations
-% RETURN :  P = matrix [p0 p1 p2... p_n]
+function p = newtons(f,df,p0,tol,maxit)
+% geschreven voor meerdere startpunten
+% f 		: filename of the function
+% df		: filename of the derivative
+% p0        : starting point : column vector [x0, y0, z0,...]'
+% tol       : value for convergence test
+% maxit	    : maximum number of iterations
+% RETURN    : P = matrix [p0 p1 p2... pn]
+% example: 
+% eerst functie definieren
+%   function v = f3(p)
+%   x = p(1); y = p(2);
+%   v = [x^2-x+y^2; x^2-y-y^2];  
+% dan afgeleide definieren
+%   function V = f3j(p)
+%   x = p(1); y = p(2);
+%   V = [2*x-1, 2*y; 2*x, -2*y-1]; 
+%newtons(@f3,@f3j,[0.8;0.4],1e-7,100)
 p = p0;
 for n = 1 : maxit
-    p(:,n+1) = p(:,n) - inv(feval(df,p(:,n))) * feval(f,p(:,n));
-    if norm(feval(f,p(:,n+1))) < delta
+    p(:,n+1) = p(:,n) - feval(df,p(:,n))\feval(f,p(:,n));
+    if norm(feval(f,p(:,n+1))) < tol % norm kleiner dan de tolerantie? klaar...
         disp('Convergence test : OK');
-        return; % stop function
+        return; % stops the function and returns p
     end
-end        
+end
 disp('Convergence test : NOT OK');
