0c2457b33f76945134cb979e1bfc2676987201e2
165_goethals <thijs.goethals@hotmail.com>
2011-12-10 16:46:34 +0100

    minor improvements in assigning startvalues

--- secant.m
+++ secant.m
@@ -1,28 +1,26 @@
-% Dit script steunt op de secant methode (quasi-newton methode)
-% fun 		: bestandsnaam van de functie (@) of inline
-% x0		: startwaarde
-% x1        : 2de startwaarde
-% tol    	: Tolerantie voor convergentie test
-% max	    : Maximum aantal iteraties
-% Geeft ook aan of functie geconvergeerd is of niet!
-
-function [x,y] = secant(fun,x0,x1,tol,max)
-x(1) = x0;
-x(2) = x1;
-y(1) = feval(fun,x(1)); 
-y(2) = feval(fun,x(2));
-    for i = 3 : max
-        x(i) = x(i-1) - y(i-1)/((y(i-1)-y(i-2))/(x(i-1)-x(i-2))); 
-        y(i) = feval(fun,x(i));
-        if abs(x(i) - x(i-1)) < tol
-            disp('Secant methode is geconvergeerd'); 
-            break;
-        end
-        if i== max
-            disp('Nulpunt niet gevonden');
-        end
+function [x,y] = secant(f,x0,x1,tol,max)
+% This script uses the secant method(quasi-newton method). The script
+% indicates if the function has converged or not.
+% f 		: filename of the function
+% x0		: starting value 1
+% x1        : starting value 2
+% delta    	: value for convergence test
+% maxit	    : maximum number of iterations
+% x         : output x-values
+% y         : output y-values
+%example: secant(y,3,400,10^-10,100) (y=inline(f,'x')
+f=vectorize(f);
+x = [x0 x1];
+y = feval(f,x); 
+for i = 3 : max
+    x(i) = x(i-1) - y(i-1)/((y(i-1)-y(i-2))/(x(i-1)-x(i-2))); 
+    y(i) = feval(f,x(i));
+    if abs(x(i) - x(i-1)) < tol
+        disp('Secant methode is geconvergeerd');
+        disp([ (1:length(x))' x' y']); 
+        % 1e kolom: teller; x: x-waarde; y: overeenkomstige y-waarde
+        return;
     end
-n = length(x); 
-k = 1:n; 
-out = [k' x' y'];   % k: teller; x: x-waarde; y: overeenkomstige y-waarde
-disp(out);
+ end
+ fprintf('Nulpunt niet gevonden. Laatste waarde was %f \n',x(max));
+end
