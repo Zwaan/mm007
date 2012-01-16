f7a6cc7d8f2205fc01ea145e20b827e29bc0ef0c
Thijs Goethals <thijs.goethals@hotmail.com>
2012-01-16 19:20:39 +0100

    changed newtons so input can be simplified. This using cell argument in the feval function.

--- newtons.m
+++ newtons.m
@@ -1,5 +1,6 @@
 function p = newtons(f,df,p0,tol,maxit)
-% geschreven voor meerdere startpunten
+% newtons solves systems of multiple lineair equations. It can handle
+% multiple startpoints.
 % f 		: filename of the function
 % df		: filename of the derivative
 % p0        : starting point : column vector [x0, y0, z0,...]'
@@ -7,21 +8,21 @@ function p = newtons(f,df,p0,tol,maxit)
 % maxit	    : maximum number of iterations
 % RETURN    : P = matrix [p0 p1 p2... pn]
 % example: 
-% eerst functie definieren
-%   function v = f3(p)
-%   x = p(1); y = p(2);
-%   v = [x^2-x+y^2; x^2-y-y^2];  
-% dan afgeleide definieren
-%   function V = f3j(p)
-%   x = p(1); y = p(2);
-%   V = [2*x-1, 2*y; 2*x, -2*y-1]; 
-%newtons(@f3,@f3j,[0.8;0.4],1e-7,100)
-p = p0;
+%   f=@(x,y) [x^2-x+y^2; x^2-y-y^2];  
+%   df=@(x,y) [2*x-1, 2*y; 2*x, -2*y-1]; 
+%newtons(f,df,[0.8;0.4],1e-7,100)
+p =zeros(2,maxit);
+p(:,1)=p0;
 for n = 1 : maxit
-    p(:,n+1) = p(:,n) - feval(df,p(:,n))\feval(f,p(:,n));
-    if norm(feval(f,p(:,n+1))) < tol % norm kleiner dan de tolerantie? klaar...
+    arg=num2cell(p(:,n));
+    p(:,n+1) = p(:,n) - feval(df,arg{:})\feval(f,arg{:});
+    arg2=num2cell(p(:,n+1));
+    %if norm smaller then tollerance the program finishes.
+    if norm(feval(f,arg2{:})) < tol
         disp('Convergence test : OK');
+        p=p(:,1:n);
         return; % stops the function and returns p
     end
 end
 disp('Convergence test : NOT OK');
+end
