294a33fc6263f6e190b39c3b44751c83493686cb
Thijs Goethals <thijs.goethals@hotmail.com>
2012-01-16 20:28:46 +0100

    eval replaced by evalv because eval was matlab reserved

--- bisection.m
+++ bisection.m
@@ -3,7 +3,7 @@ function [X,r]=bisection( f,a,b,delta,maxit )
 %ascending and descending functions.
 % f 		: filename of the function
 % a     	: starting value
-% b         : secon begin value
+% b         : second begin value
 % delta 	: value for convergence test (eps)
 % maxit	    : maximum number of iterations
 % RETURN    : X = vector [x0 x1 x2... xn], r=vector with info over
@@ -12,16 +12,16 @@ function [X,r]=bisection( f,a,b,delta,maxit )
 %example: bisection(inline('x^3-3*x^2+1','x'),0,1,0.0001,100)
 val=[a .5*a+.5*b b];
 f=vectorize(f);
-eval= feval(f,val);
+evalv= feval(f,val);
 % evaluates sign change
-if sign(eval(1))==sign(eval(3)) 
+if sign(evalv(1))==sign(evalv(3)) 
     error('no sign change');
 end
 r=zeros(maxit,3);
 for n = 1 : maxit
-  r(n,1:3)=[n val(2) eval(2)];
+  r(n,1:3)=[n val(2) evalv(2)];
   %stops if within toleration
-    if abs(eval(2))<=delta
+    if abs(evalv(2))<=delta
         disp('    count    value    evaluation');
         disp(r);
         fprintf('zero within tolerance %f in %f', delta, val(2)); X=r(:,2);
@@ -30,16 +30,16 @@ for n = 1 : maxit
     %right bound. If signchange between left bound and midpoint then new
     %right bound is current midpoint. If no signchange an error is
     %displayed
-    elseif eval(1)*eval(2)<0&&eval(3)*eval(2)>0
-        val(3)=val(2);eval(3)=eval(2);
-    elseif eval(1)*eval(2)>0&&eval(3)*eval(2)<0
-        val(1)=val(2);eval(1)=eval(2);
-    elseif sign(eval(3))==sign(eval(1))
+    elseif evalv(1)*evalv(2)<0&&evalv(3)*evalv(2)>0
+        val(3)=val(2);evalv(3)=evalv(2);
+    elseif evalv(1)*evalv(2)>0&&evalv(3)*evalv(2)<0
+        val(1)=val(2);evalv(1)=evalv(2);
+    elseif sign(evalv(3))==sign(evalv(1))
         fprintf('multiple zeros between %f and %f', val(1), val(3));return;
     end
     %calculates new midpoint and evaluates function in the new midpoint.
     val(2)=.5*val(1)+.5*val(3);
-    eval(2)=feval(f,val(2));
+    evalv(2)=feval(f,val(2));
 end
 disp('Convergence test : NOT OK');
 end
