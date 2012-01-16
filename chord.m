4d0e66621cfa6bd35f24fb43f0c25561289165c5
Thijs Goethals <thijs.goethals@hotmail.com>
2012-01-16 19:31:23 +0100

    renamed koorde -> chord and added some optimasations

--- chord.m
+++ chord.m
@@ -1,4 +1,4 @@
-function [x,y] = koorde(f,df,x0,delta,maxit)
+function [x,y] = chord(f,df,x0,delta,maxit)
 % f 		: filename of the function
 % df		: filename of the derivative
 % x0		: starting value
@@ -9,19 +9,18 @@ function [x,y] = koorde(f,df,x0,delta,maxit)
 %dy=inline('3*x^2+3','x'); 
 %K=koorde(y,dy,3,10^-20,aantal);
 
-x(1) = x0;
-y(1) = feval(f,x(1)); 
+x(1:maxit) = x0;
+y(1:maxit) = feval(f,x(1)); 
 dy = feval(df,(x(1)));
 
 for i = 2 : maxit
     x(i) = x(i-1) - y(i-1)/dy; 
     y(i)=feval(f,x(i));
     if abs(x(i) - x(i-1)) < delta
-        disp('Koorde: Method converged'); break;
+        disp('Koorde: Method converged');
+        n = length(x); k = 1:n; out = [k' x' y']; disp(out);
+        return
     end
-   
-    if i== maxit
-        disp('Koorde: Zero not found');
-    end
+ end
+ disp('Koorde: Zero not found');
 end
-n = length(x); k = 1:n; out = [k' x' y']; disp(out);
