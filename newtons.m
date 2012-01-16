299ac02b7c8faf3e6d94107c6045b0cde2689a67
Thijs Goethals <thijs.goethals@hotmail.com>
2012-01-16 21:47:06 +0100

    updated comments and return adapted

--- newtons.m
+++ newtons.m
@@ -8,9 +8,17 @@ function p = newtons(f,df,p0,tol,maxit)
 % maxit	    : maximum number of iterations
 % RETURN    : P = matrix [p0 p1 p2... pn]
 % example: 
+%without m-files
 %   f=@(x,y) [x^2-x+y^2; x^2-y-y^2];  
 %   df=@(x,y) [2*x-1, 2*y; 2*x, -2*y-1]; 
-%newtons(f,df,[0.8;0.4],1e-7,100)
+%   newtons(f,df,[0.8;0.4],1e-7,100)
+%with m-files
+%   function v = f3(x,y)
+%   v = [x^2-x+y^2; x^2-y-y^2];
+%derivative
+% function V = f3j(x,y)
+%   V = [2*x-1, 2*y; 2*x, -2*y-1];
+%newtons(@f3,@f3j,[0.8;0.4],1e-7,100)
 p =zeros(2,maxit);
 p(:,1)=p0;
 for n = 1 : maxit
@@ -20,7 +28,7 @@ for n = 1 : maxit
     %if norm smaller then tollerance the program finishes.
     if norm(feval(f,arg2{:})) < tol
         disp('Convergence test : OK');
-        p=p(:,1:n);
+        p=p(:,1:n+1);
         return; % stops the function and returns p
     end
 end
