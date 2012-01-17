337661b86827370d8d44f97c36a816585dd1d5e2
Thijs Goethals <thijs.goethals@hotmail.com>
2012-01-17 22:25:09 +0100

    added comments on return. Updated variable usage cfr. course. Some little optimasations tweaks using backslash.

--- seidel.m
+++ seidel.m
@@ -1,10 +1,12 @@
-function seidel(A,b,x0,delta,maxit)
+function [x,iter,T]=seidel(A,b,x0,delta,maxit)
 % this function solves a n-by-n linear system A x = b
 % with Gauss-Seidel iterative method
 % Ax=b      : the system
 % x0		: initial solution
 % delta 	: value for convergence test
 % maxit     : maximum number of iterations
+% RETURN    : x: final solution, iter: complete iteration, 
+%             T: Gauss-Seidel-iteration-matrix.
 
 %vb: A=[-4 1 1 0; 1 -4 0 1;1 0 -4 1; 0 1 1 -4];b=[-3;-1;-5;-3];
 % seidel(A,b,[1;0;0;0],1e-5,100)
@@ -16,32 +18,31 @@ if h>100
 end
 % construction of J and K
 % Dinv = inv(diag(diag(A))); %% diag(diag(A)) is nodig om terug een 3*3 te hebben ipv een 3*1 matrix
-E = -tril(A,-1);% onderdriehoeksmatrix min de 1ste diagonaal
-F = -triu(A,1);% bovendriehoeksmatrix min de 1ste diagonaal
-J = diag(diag(A))\(E+F);
-K = diag(diag(A))\b;
-
-M=diag(diag(A))+ tril(A,-1);N=-triu(A,1); %p 3/5 H3 M=D+L en N=-U
-
-if max(eig(M\N))<1 % NVV convergentie p3/5 H3
-
+L = tril(A,-1);% onderdriehoeksmatrix min de 1ste diagonaal
+U = triu(A,1);% bovendriehoeksmatrix min de 1ste diagonaal
+D = diag(diag(A));
+J=-D\(L+U);
+M=D+L;N=-U;%p 3/5 H3 M=D+L en N=-U
+K=D\b;%K=inv(D)*b
+T=M\N;%Gauss-Seidel-iteration-matrix
+if max(eig(M\N))>=1 % NVV convergentie p3/5 H3
+  disp('NVV convergentie G-S niet voldaan');
+  x=x0;return;
+end
 % recurrence
 Lx = length(x0);% is langste waarde, hier dus aantal onbekenden
-x(:,1) = x0; % geeft zoizo kolommatrix, eender of x0 er ��n is of niet
+x=zeros(Lx,maxit);
+x(:,1)=x0;
 for n = 1 : maxit
     x(:,n+1) = x(:,n);
     for j = 1:Lx
         x(j,n+1) = J(j,:) * x(:,n+1) + K(j);
     end
     if norm(x(:,n+1) - x(:,n))/norm(x(:,n)) < delta % relatieve fout nakijken
-        disp('convergence test : OK'); % nu x, x(n) en n weergeven
-        fprintf('n=%d',n);
-        fprintf('\nx(n)= \n');disp(x(:,n));x 
+        iter=x(:,1:n+1);x=x(:,n+1);
+        fprintf('convergence test : OK\n n=%d\n',n); 
         return;
     end
 end
 disp('convergence test : NOT OK');
-else
-    disp('NVV convergentie G-S niet voldaan');
-end
 end
